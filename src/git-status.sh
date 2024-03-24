5ec5913978a0cb0d49c60183da954ab5c77c3746
Radvil <radvil.linux@gmail.com>
2024-03-24 22:59:42 +0800

    update git colors

--- src/git-status.sh
+++ src/git-status.sh
@@ -5,11 +5,11 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 STATUS=$(git status --porcelain 2>/dev/null | egrep "^(M| M)" | wc -l)
 if test "$BRANCH" != ""; then
 	if test "$STATUS" = "0"; then
-		echo "#[fg=#89b4fa,bg=#45475a,nobold,nounderscore,noitalics]#[fg=#15161e,bg=#89b4fa,bold]  $BRANCH"
+		echo "#[fg=#5d81ab,bg=#30303b,nobold,nounderscore,noitalics]#[fg=#202027,bg=#5d81ab,bold]  $BRANCH"
 	else
-		echo "#[fg=#f5e0dc,bg=#45475a,nobold,nounderscore,noitalics]#[fg=#15161e,bg=#f5e0dc,bold]  $BRANCH"
+		echo "#[fg=#f5e0dc,bg=#30303b,nobold,nounderscore,noitalics]#[fg=#202027,bg=#f5e0dc,bold]  $BRANCH"
 	fi
 else
 	# os distro
-	echo "#[fg=#89b4fa,bg=#45475a,nobold,nounderscore,noitalics]#[fg=#15161e,bg=#89b4fa,bold] #h"
+	echo "#[fg=#5d81ab,bg=#30303b,nobold,nounderscore,noitalics]#[fg=#202027,bg=#5d81ab,bold] #h"
 fi
